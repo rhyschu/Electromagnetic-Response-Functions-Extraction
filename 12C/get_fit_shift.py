a78714e91437e43c403ef0daa0aabb66f8b07148
rhyschu <rabirabo194@gmail.com>
2025-04-27 20:41:28 -0400

    04/27/2025

--- 12C/get_fit_shift.py
+++ 12C/get_fit_shift.py
@@ -1,27 +1,27 @@
 import pandas as pd
 import subprocess
 
-data = '56Fe.csv'
-data_fit = '56Fe_Fit.csv'
+data = 'Fe56.csv'
+data_fit = 'Fe56_Fit.csv'
 elem= '56Fe'
 elem_dict = {
-    "1H": {"A": 1, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 1/6, "QENS_shift": 0, "NS_factor": 1},
-    "2H": {"A": 2, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 1/6, "QENS_shift": 0, "NS_factor": 1},
-    "12C": {"A": 12, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 1, "QENS_shift": 0, "NS_factor": 1},
-    "40Ca": {"A": 40, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 20/6, "QENS_shift": 0, "NS_factor": 1},
-    "56Fe": {"A": 56, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 26/6, "QENS_shift": 0.013, "NS_factor": 0.5}
+    "1H": {"A": 1, "ex_cut_lower": 0, "ex_cut_upper": 1000, "multiplier": 1/6, "QENS_shift": 0, "NS_factor": 1},
+    "2H": {"A": 2, "ex_cut_lower": 0, "ex_cut_upper": 1000, "multiplier": 1/6, "QENS_shift": 0, "NS_factor": 1},
+    "12C": {"A": 12, "ex_cut_lower": 0, "ex_cut_upper": 1000, "multiplier": 1, "QENS_shift": 0, "NS_factor": 1},
+    "40Ca": {"A": 40, "ex_cut_lower": 0, "ex_cut_upper": 1000, "multiplier": 20/6, "QENS_shift": 0, "NS_factor": 1},
+    "56Fe": {"A": 56, "ex_cut_lower": 0, "ex_cut_upper": 1000, "multiplier": 26/6, "QENS_shift": 0.013, "NS_factor": 0.5}
 }
 
 if __name__ == "__main__":
     df = pd.read_csv(data)
     with open(data_fit, "w") as response_file:
-        response_file.write("Data:Z,A,E0,ThetaDeg,nu,cross,error,dataSet,Fit:ep,theta,nu,Ex,w2,q2,sigtot,sigqe,sigie,sigmec,nuccstot,signonuc,sigtot_shifted,qe_shifted,ns_shifted,ratio\n")
+        response_file.write("Data:Z,A,E0,ThetaDeg,nu,cross,error,dataSet,Fit:ep,theta,nu,Ex,W2,q2,sigtot,sigqe,sigie,sigmec,nuccstot,signonuc,sigtot_shifted,qe_shifted,mec_shifted,ns_shifted,ratio\n")
     for index, row in df.iterrows():
         Z = row['Z']
         A = row['A']
         E0 = row['E0']
         ThetaDeg = row['ThetaDeg']
-        nu = row['nu']   
+        nu = row['nu']
         cross = row['cross']
         error = row['error']
         dataSet = row['dataSet']
@@ -46,14 +46,24 @@ if __name__ == "__main__":
         for i in range(6, 12):
             values[i] = float(values[i]) * 1000 * 12 * elem_dict[elem]["multiplier"]
             values_shifted[i] = float(values_shifted[i]) * 1000 * 12 * elem_dict[elem]["multiplier"]
-        sigtot_shifted = values[6] - values[7] + values_shifted[7] - values[10] + values_shifted[10] * elem_dict[elem]["NS_factor"]
+        ep = values[0]
+        theta = values[1]
+        nu_fit = values[2]
+        Ex = float(values[3])
+        W2 = values[4]
+        q2 = values[5]
+        sigtot = values[6]
+        sigqe = values[7]
+        sigie = values[8]
+        sigmec = values[9]
+        nuccstot = values[10]
+        signonuc = values[11]
+        sigtot_shifted = values[6] - values[7] + values_shifted[7] - values[9] + values_shifted[9] - values[10] + values_shifted[10] * elem_dict[elem]["NS_factor"]
         qe_shifted = values_shifted[7]
+        mec_shifted = values_shifted[9]
         ns_shifted = values_shifted[10] * elem_dict[elem]["NS_factor"]
-        values.append(sigtot_shifted)
-        values.append(qe_shifted)
-        values.append(ns_shifted)
-        if sigtot_shifted == 0 or float(values[3]) < elem_dict[elem]["ex_cut_lower"] or float(values[3]) > elem_dict[elem]["ex_cut_upper"]:
+        if sigtot_shifted == 0 or Ex < elem_dict[elem]["ex_cut_lower"] or Ex > elem_dict[elem]["ex_cut_upper"]:
             continue
-        values.append(float(cross) / sigtot_shifted)
+        ratio = float(cross) / sigtot_shifted
         with open(data_fit, "a") as response_file:
-            response_file.write(f"{Z},{A},{E0},{ThetaDeg},{nu},{cross},{error},{dataSet}," + ",".join(map(str, values)) + "\n")
+            response_file.write(f"{Z},{A},{E0},{ThetaDeg},{nu},{cross},{error},{dataSet},{ep},{theta},{nu_fit},{Ex},{W2},{q2},{sigtot},{sigqe},{sigie},{sigmec},{nuccstot},{signonuc},{sigtot_shifted},{qe_shifted},{mec_shifted},{ns_shifted},{ratio}\n")
--- 12C/plot_cross_shift.py
+++ 12C/plot_cross_shift.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.ticker import ScalarFormatter
 
-data = '56Fe.csv'
-data_fit = '56Fe_Fit.csv'
-data_SuSAV2 = '56Fe_SuSAV2.csv'
+data = 'df_Fe56.csv'
+data_fit = 'Fe56_Fit.csv'
+data_SuSAV2 = 'df_Fe56_SuSAV2_Extraction.csv'
 pdf_file = "Fe56_Cross_Section.pdf"
 elem= '56Fe'
 elem_dict = {
@@ -13,16 +13,17 @@ elem_dict = {
     "2H": {"A": 2, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 1/6, "QENS_shift": 0, "NS_factor": 1},
     "12C": {"A": 12, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 1, "QENS_shift": 0, "NS_factor": 1},
     "40Ca": {"A": 40, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 20/6, "QENS_shift": 0, "NS_factor": 1},
-    "56Fe": {"A": 56, "ex_cut_lower": 0.03, "ex_cut_upper": 1000, "multiplier": 26/6, "QENS_shift": 0.013, "NS_factor": 0.5}
+    "56Fe": {"A": 56, "ex_cut_lower": 0, "ex_cut_upper": 1000, "multiplier": 26/6, "QENS_shift": 0.013, "NS_factor": 0.5}
 }
-mass_nucleus = elem_dict[elem]["A"]*0.931494
+mass_nucleus = elem_dict[elem]["A"] * 0.931494
+x_axis_nu = False
 df = pd.read_csv(data)
 df_fit = pd.read_csv(data_fit)
 df_SuSAV2 = pd.read_csv(data_SuSAV2)
 df_SuSAV2 = df_SuSAV2[df_SuSAV2['cross'] != 0]
 df_SuSAV2.replace('#DIV/0!', pd.NA, inplace=True)
 df_SuSAV2.dropna(inplace=True)
-value_pairs = sorted(set((row["E0"], row["ThetaDeg"], row["dataSet"]) for _, row in df.iterrows()), key=lambda x: (x[2], x[0], x[1]))
+value_pairs = sorted(set((row["E0"], row["ThetaDeg"], row["dataSet"]) for _, row in df.iterrows()), key=lambda x: (x[2], x[1], x[0]))
 
 with PdfPages(pdf_file) as pdf:
         for i in range(len(value_pairs) // 12 + 1):
@@ -35,25 +36,41 @@ with PdfPages(pdf_file) as pdf:
                 filtered_data = df[(df['E0'] == E0) & (df['ThetaDeg'] == ThetaDeg) & (df['dataSet'] == dataSet)]
                 Z = filtered_data['Z'].iloc[0]
                 A = filtered_data['A'].iloc[0]
-                x = filtered_data['nu']  
-                y = filtered_data['cross']  
-                yerr = filtered_data['error']
-                filtered_data_fit = df_fit[(df_fit['E0'] == E0) & (df_fit['ThetaDeg'] == ThetaDeg) & (df_fit['dataSet'] == dataSet)] 
-                x_fit = filtered_data_fit['nu']  
+                if x_axis_nu:
+                    x = filtered_data['nu']
+                else:
+                    x = filtered_data['Ex']
+                y = filtered_data['normCross']
+                yerr = filtered_data['normCrossError']
+                normaliztion = filtered_data['normalization'].iloc[0]
+                filtered_data_fit = df_fit[(df_fit['E0'] == E0) & (df_fit['ThetaDeg'] == ThetaDeg) & (df_fit['dataSet'] == dataSet)]
+                if x_axis_nu:
+                    x_fit = filtered_data_fit['nu']
+                else:
+                    x_fit = filtered_data_fit['Ex']
                 y_fit = filtered_data_fit['sigtot_shifted']
+                y_fit_QE = filtered_data_fit['qe_shifted'] + filtered_data_fit['mec_shifted']
                 filtered_data_SuSAV2 = df_SuSAV2[(df_SuSAV2['E0'] == E0) & (df_SuSAV2['ThetaDeg'] == ThetaDeg) & (df_SuSAV2['dataSet'] == dataSet)]
-                x_SuSAV2 = filtered_data_SuSAV2['nu']
+                if x_axis_nu:
+                    x_SuSAV2 = filtered_data_SuSAV2['nu']
+                else:
+                    x_SuSAV2 = filtered_data_SuSAV2['Ex']
                 y_SuSAV2 = filtered_data_SuSAV2['cross']
-
-                ax.errorbar(x, y, yerr=yerr, fmt='.', label=f'$E_0=${E0}$GeV$\n$\\theta=${ThetaDeg}°', color='blue', zorder=-1)
-                ax.scatter(x_fit, y_fit, label='Christy-Bodek Fit', color='red', marker='.')
-                ax.plot(x_fit, y_fit, color='red', alpha=0.5)
+                
+                ax.errorbar(x, y, yerr=yerr, fmt='.', label=f'normCross', color='blue', zorder=-1)
+                # ax.scatter(x_fit, y_fit, label='Christy-Bodek Fit Total', color='red', marker='.')
+                # ax.plot(x_fit, y_fit, color='red', alpha=0.5)
+                ax.scatter(x_fit, y_fit_QE, label='Christy-Bodek Fit QE', color='red', marker='.')
+                ax.plot(x_fit, y_fit_QE, color='red', alpha=0.5, linestyle='--')
                 ax.scatter(x_SuSAV2, y_SuSAV2, label='SuSAV2', color='lightgreen', marker='.')
                 ax.plot(x_SuSAV2, y_SuSAV2, color='lightgreen', alpha=0.5)
-                ax.set_xlabel('$\\nu \ (GeV)$')    
+                if x_axis_nu:
+                    ax.set_xlabel('$\\nu \ (GeV)$')
+                else:
+                    ax.set_xlabel('$Ex \ (GeV)$')
                 ax.set_ylabel('$\\frac{d^2 \sigma}{d\Omega d\\nu} (nb/sr/GeV)$')
                 ax.set_ylim(0, None)
-                ax.set_title(f'{dataSet} {E0}$GeV$ {ThetaDeg}°')
+                ax.set_title(f'{dataSet} {E0}$GeV$ {ThetaDeg}° (X {normaliztion})')
                 formatter = ScalarFormatter(useMathText=True)
                 formatter.set_scientific(True)
                 formatter.set_powerlimits((0,0))
